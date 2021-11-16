3775d174c20d9b2ab86ac262cf89605f957b2321
Grzegorz46 <87185934+Grzegorz46@users.noreply.github.com>
2021-11-16 14:26:24 +0100

    Update and rename erc20_tutorial.sol to erc20_prometheus.sol

--- contracts/erc20_prometheus.sol
+++ contracts/erc20_prometheus.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'Prometheus' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
+// Deployed to : 0x12709010bA0bd4B5b5750E4D3989aE323d8DF2eE
+// Symbol      : PROM
+// Name        : Prometheus Token
+// Total supply: 100000000000000000
 // Decimals    : 18
 //
 // Enjoy.
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract PrometheusToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+        symbol = "PROM";
+        name = "Prometheus Token";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 1000000000000000000000;
+        balances[0x12709010bA0bd4B5b5750E4D3989aE323d8DF2eE] = _totalSupply;
+        emit Transfer(address(0), 0x12709010bA0bd4B5b5750E4D3989aE323d8DF2eE, _totalSupply);
     }
 
 
